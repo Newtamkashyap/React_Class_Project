56f2d4e52c1d821822bef69df85cca21b6ba3c26
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-24 16:01:31 +0530

    button added

--- src/loginComponent/login.jsx
+++ src/loginComponent/login.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 
 
 export function UserLogin(){
+
+// var title="Admin login";
+// var uname="john";
+
+const [title,setTitle]=useState("Admin Name");
+const [name,setName]=useState("john")
+
     return(
       <div className="container-fluid">
         <h2>Bootstrap in react</h2>
@@ -9,13 +17,13 @@ export function UserLogin(){
             <div className="modal-dialog modal-dailog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h2>UserLogin</h2>
+                        <h2 className="bi bi-person-fill">{title}</h2>
                         <button data-bs-dismiss="modal" className="btn btn-close"></button>
                     </div>
                     <div className="modal-body">
                         <dl>
-                            <dt>User Name</dt>
-                            <dd><input type="text" className="form-control"/></dd>
+                            <dt >User Name</dt>
+                            <dd><input type="text" value={name.toUpperCase()} className="form-control" /></dd>
                             <dt>Password</dt>
                             <dd><input type="password" className="form-control"/></dd>
                         </dl>
