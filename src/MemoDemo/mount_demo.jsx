3a93670eef2c78a511b9e26d7ca6601411ae1ca1
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-27 17:20:43 +0530

    examples

--- src/MemoDemo/mount_demo.jsx
+++ src/MemoDemo/mount_demo.jsx
@@ -1,16 +1,60 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
-export const MountDemo=()=>{
+export const MountDemo = () => {
 
-     const [name,setName]=useState('Newtam')
+    const [products, setProducts] = useState([{ Name: '', Price: 0, Stock: false }]);
 
-     function demo(e){
-        setName(e.target.value)
-     }
-    return(
-        <div>
-            <p onChange={demo} value={name}>Hello !{name}</p>
+    function LoadProducts() {
+        // var http = new XMLHttpRequest();
+        // http.open("get", "product.json", true); //get is mandatory for getting data,it is showing what type of request is GET,POSt,DELETE,Update..like that
+        // http.send();
+
+        // http.onreadystatechange = function () {
+        //     if (http.readyState == 4) {
+        //         setProducts(JSON.parse(http.responseText));
+        //     }
+        // }
+
+        //========================OR(we can do with fetch method,easy and sort)=======================
+
+        fetch("product.json")
+        .then(response => response.json())
+        .then(products =>{
+            (setProducts(products));
+        })
+    }
+
+    useEffect(() => {
+        LoadProducts();
+    })
+
+    return (
+        <div className="container-fluid">
+            <h2>Products Table</h2>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>price</th>
+                        <th>Stock</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product =>
+                            <tr key={product.Name}>
+                                <td>{product.Name}</td>
+                                <td>{product.Price}</td>
+                                <td>{(product.Stock===true)?"Available":"out of stock"}</td>
+
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+
         </div>
     )
 }
