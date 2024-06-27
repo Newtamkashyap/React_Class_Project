67d1c02eab3bb4d804009d6b767715923fd9efaf
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-27 15:55:16 +0530

    example of useEffect

--- src/MemoDemo/mount_demo.jsx
+++ src/MemoDemo/mount_demo.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 
 export const MountDemo=()=>{
 
-     const [name,setName]=useState('')
+     const [name,setName]=useState('Newtam')
 
      function demo(e){
         setName(e.target.value)
      }
     return(
         <div>
-            <p onChange={demo}>Hello !{name}</p>
+            <p onChange={demo} value={name}>Hello !{name}</p>
         </div>
     )
 }
