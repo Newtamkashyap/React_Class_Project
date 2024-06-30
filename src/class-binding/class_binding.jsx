c7e7b78ced60da442d2b3abd0fddec538aebeb1d
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-30 18:10:35 +0530

    .

--- src/class-binding/class_binding.jsx
+++ src/class-binding/class_binding.jsx
@@ -4,24 +4,24 @@ import { useState } from "react"
 export const ClassBinding = () => {
 
     const [theme, setTheme] = useState('');
-    const [buttonClass, setButtonClass] = useState(' ');
+    const [buttonClass, setButtonClass] = useState('btn btn-dark w-100 ');
 
     function handleThemeChange(e) {
         if (e.target.checked) {
             setTheme("bg-dark text-white");
-            setButtonClass('btn btn-light ');
+            setButtonClass('btn btn-light w-100 ');
         }
         else {
             setTheme("bg-light text-dark");
-            setButtonClass('btn btn-dark ');
+            setButtonClass('btn btn-dark w-100 ');
         }
 
     }
 
 
     return (
-        <div className="container-fluid d-flex justify-content-center align-items-center mt-5">
-            <form className={`border border-1 rounded rounded-2 p-4 ${theme} ${buttonClass}`}>
+        <div className="container-fluid d-flex justify-content-center align-items-center mt-5 w-100">
+            <form className={`border border-1 rounded rounded-2 p-4 ${theme}`}>
                 <h2 className="bi bi-person-fill">User login</h2>
                 <div className="form-switch">
                     <input type='checkbox' onChange={handleThemeChange} className="form-check-input" /> Dark Mode
@@ -38,9 +38,9 @@ export const ClassBinding = () => {
                         <input type='password' className="form-control" />
                     </div>
                 </div>
-                <div>
+                <div className="my-2">
 
-                    <button className={buttonClass}>Button</button>
+                    <button className={buttonClass} >Button</button>
 
                 </div>
             </form>
