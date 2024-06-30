12a781984ec26129b4074dffca136353f71d4b82
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-30 18:27:41 +0530

    adding bootstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
