f008896f49f07630df6fde61221aae974f96e537
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-06-30 17:30:14 +0530

    classBinding-example

--- src/class-binding/class_binding.jsx
+++ src/class-binding/class_binding.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react"
 
 
-export const ClassBinding=()=>{
-    return(
-        <>
-        </>
+export const ClassBinding = () => {
+
+    const [theme, setTheme] = useState('');
+    const [buttonClass, setButtonClass] = useState(' ');
+
+    function handleThemeChange(e) {
+        if (e.target.checked) {
+            setTheme("bg-dark text-white");
+            setButtonClass('btn btn-light ');
+        }
+        else {
+            setTheme("bg-light text-dark");
+            setButtonClass('btn btn-dark ');
+        }
+
+    }
+
+
+    return (
+        <div className="container-fluid d-flex justify-content-center align-items-center mt-5">
+            <form className={`border border-1 rounded rounded-2 p-4 ${theme} ${buttonClass}`}>
+                <h2 className="bi bi-person-fill">User login</h2>
+                <div className="form-switch">
+                    <input type='checkbox' onChange={handleThemeChange} className="form-check-input" /> Dark Mode
+                </div>
+                <div className="my-2" >
+                    <label className="form-label">User name</label>
+                    <div>
+                        <input type='text' className="form-control" />
+                    </div>
+                </div>
+                <div className="my-2">
+                    <label className="form-label">Password</label>
+                    <div>
+                        <input type='password' className="form-control" />
+                    </div>
+                </div>
+                <div>
+
+                    <button className={buttonClass}>Button</button>
+
+                </div>
+            </form>
+        </div>
     )
 }
