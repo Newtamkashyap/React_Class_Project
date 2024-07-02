9f6d7358ed29bdd2d5a5cc97d7a9a0d66ed69cff
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-07-02 11:04:51 +0530

    mui-example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { UIDemo } from './Mui-components/ui-demo';
+import ButtonAppBar from './Mui-components/appBar';
+import BasicGrid from './grid';
+import RecipeReviewCard from './Mui-components/card';
 
 function App() {
   return (
     <>
 
     <UIDemo/>
+    <ButtonAppBar/>
+    <BasicGrid/>
+    <RecipeReviewCard/>
     </>
   );
 }
